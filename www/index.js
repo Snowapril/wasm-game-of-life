166d965e7f4772e845d7b99423b97ac1a0e10112
snowapril <sinjihng@gmail.com>
2021-09-28 18:54:35 +0900

    feat: use wasm-memory directly for rendering

--- www/index.js
+++ www/index.js
@@ -1,12 +1,75 @@
-import { Universe } from "wasm-game-of-life";
+import { Universe, Cell } from "wasm-game-of-life";
+import { memory } from "wasm-game-of-life/wasm_game_of_life_bg";
 
-const pre = document.getElementById("game-of-life-canvas");
+const CELL_SIZE = 10;
+const GRID_COLOR = "#CCCCCC";
+const DEAD_COLOR = "#FFFFFF";
+const ALIVE_COLOR = "#000000";
+
 const universe = Universe.new();
+const width = universe.width();
+const height = universe.height();
+
+const canvas = document.getElementById("game-of-life-canvas");
+canvas.width = (CELL_SIZE + 1) * width + 1;
+canvas.height = (CELL_SIZE + 1)  * height + 1;
+
+const ctx = canvas.getContext("2d");
+
+const getIndex = (x, y) => {
+    return y * width + x;
+};
+
+const drawGrid = () => {
+    ctx.beginPath();
+
+    ctx.strokeStyle = GRID_COLOR;
+    for (let i = 0; i <= width; i++) {
+        ctx.moveTo(i * (CELL_SIZE + 1) + 1, 0);
+        ctx.lineTo(i * (CELL_SIZE + 1) + 1, (CELL_SIZE + 1) * height + 1);
+    }
+
+    for (let j = 0; j <= height; j++) {
+        ctx.moveTo(0, j * (CELL_SIZE + 1) + 1);
+        ctx.lineTo((CELL_SIZE + 1) * width + 1, j * (CELL_SIZE + 1) + 1);
+    }
+
+    ctx.stroke();
+};
+
+const drawCells = () => {
+    const cellsPtr = universe.cells();
+    const cells = new Uint8Array(memory.buffer, cellsPtr, width * height);
+
+    ctx.beginPath();
+
+    for (let y = 0; y < height; y++) {
+        for (let x = 0; x < width; x++) {
+            const idx = getIndex(x, y);
+            ctx.fillStyle = cells[idx] === Cell.Dead
+                ? DEAD_COLOR
+                : ALIVE_COLOR;
+            ctx.fillRect(
+                x * (CELL_SIZE + 1) + 1,
+                y * (CELL_SIZE + 1) + 1,
+                CELL_SIZE,
+                CELL_SIZE
+            );
+        }
+    }
+
+    ctx.stroke();
+};
 
 const renderLoop = () => {
-    pre.textContent = universe.render();
     universe.tick();
+
+    drawGrid();
+    drawCells();
+
     requestAnimationFrame(renderLoop);
 };
 
+drawGrid();
+drawCells();
 requestAnimationFrame(renderLoop);
